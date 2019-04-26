e6e42882590e19a001949ba7b215d4f1ecc9d2f6
kevink98 <kevin.knoeller@live.de>
2019-04-26 18:32:42 +0200

    Ini spec palletExtended

---
+++ specializations/PalletExtended.lua
@@ -0,0 +1,51 @@
+-- 
+-- GlobalCompany - Vehicles - PalletExtended
+-- 
+-- @Interface: 1.2.0.1 b3264
+-- @Author: LS-Modcompany / kevink98
+-- @Date: 26.04.2019
+-- @Version: 1.0.0.0
+-- 
+-- @Support: LS-Modcompany
+-- 
+-- Changelog:
+--		
+-- 	v1.0.0.0 (31.12.2018):
+-- 		- initial fs19 (kevink98)
+-- 
+-- Notes:
+-- 
+-- 
+-- ToDo:
+-- 
+
+local debugIndex = g_company.debug:registerScriptName("GlobalCompany-Vehicles-PalletExtended");
+
+PalletExtended = {};
+
+function PalletExtended.initSpecialization()
+    
+end;
+
+function PalletExtended.prerequisitesPresent(specializations)
+    return true;
+end;
+
+function PalletExtended.registerEventListeners(vehicleType)
+	SpecializationUtil.registerEventListener(vehicleType, "onLoad", PalletExtended);
+    SpecializationUtil.registerFunction(vehicleType, "getFillTyp", PalletExtended.getFillTyp);
+    SpecializationUtil.registerFunction(vehicleType, "getFillLevel", PalletExtended.getFillLevel);
+end;
+
+function PalletExtended:onLoad(savegame)
+	self.fillTyp = getXMLString(self.xmlFile, "vehicle.palletExtended#fillType");
+	self.fillLevel = getXMLString(self.xmlFile, "vehicle.palletExtended#fillLevel");
+end;
+
+function PalletExtended:getFillTyp()
+    return self.fillTyp;
+end;
+
+function PalletExtended:getFillLevel()
+    return self.fillLevel;
+end;
