98ce1a38b40e71ba5b5a3b80d6dd1c850830f75e
aPuehri <48010391+aPuehri@users.noreply.github.com>
2019-06-01 01:15:40 +0200

    changed debugPrint,added Multiplayer-Support,improved ObjectDetection

--- additionals/GC_ObjectInfo.lua
+++ additionals/GC_ObjectInfo.lua
@@ -3,12 +3,17 @@
 --
 -- @Interface: --
 -- @Author: LS-Modcompany / aPuehri
--- @Date: 08.03.2019
--- @Version: 1.0.0.0
+-- @Date: 31.05.2019
+-- @Version: 1.0.1.0
 --
 -- @Support: LS-Modcompany
 --
 -- Changelog:
+--
+-- 	v1.0.1.0 (31.05.2019)/(aPuehri):
+-- 		- changed debugPrint
+-- 		- added Multiplayer-Support
+-- 		- improved ObjectDetection
 --
 -- 	v1.0.0.0 (08.03.2019):
 -- 		- initial fs19 (aPuehri)
@@ -27,15 +32,16 @@ local GC_ObjectInfo_mt = Class(GC_ObjectInfo);
 InitObjectClass(GC_ObjectInfo, "GC_ObjectInfo");
 
 GC_ObjectInfo.debugIndex = g_company.debug:registerScriptName("Gc_ObjectInfo");
-
+GC_ObjectInfo.mpFoundBale = nil;
 
 function GC_ObjectInfo:init()
 	local self = setmetatable({}, GC_ObjectInfo_mt);
 
 	self.isServer = g_server ~= nil;
 	self.isClient = g_client ~= nil;
-	
-	self.debugPrintDone = false;
+	self.isMultiplayer = g_currentMission.missionDynamicInfo.isMultiplayer;	
+
+	self.debugPrintObjectId = 0;
 	
 	self.debugData = g_company.debug:getDebugData(GC_ObjectInfo.debugIndex, g_company);
 
@@ -49,17 +55,18 @@ function GC_ObjectInfo:init()
 end;
 
 function GC_ObjectInfo:update(dt)
-	if self.isClient and g_company.settings:getSetting("objectInfo", true) then
+	if self.isClient and not (self.isServer and self.isMultiplayer) and g_company.settings:getSetting("objectInfo", true) then
 		self.showInfo = false;
+		GC_ObjectInfo.mpFoundBale = nil;
 		if g_currentMission.player.isControlled and not g_currentMission.player.isCarryingObject then
 			if g_currentMission.player.isObjectInRange then
 				if (g_currentMission.player.lastFoundObject ~= nil) then
 					local foundObjectId = g_currentMission.player.lastFoundObject;
-					if not self.debugPrintDone then
+					if (foundObjectId ~= self.debugPrintObjectId) then
 						g_company.debug:writeDevDebug(self.debugData, "New foundObjectId = %s", foundObjectId);
 					end;
 					if (foundObjectId ~= g_currentMission.terrainDetailId) then	
-						if getRigidBodyType(foundObjectId) == "Dynamic" then
+						if (getRigidBodyType(foundObjectId) == "Dynamic") then
 							local object = g_currentMission:getNodeObject(foundObjectId);
 							if (object~= nil) then
 								if (object.fillType ~= nil) and (object.fillLevel ~= nil) then
@@ -80,17 +87,16 @@ function GC_ObjectInfo:update(dt)
 										end;
 									end;
 								end;
-								self.debugPrintDone = true;
+								self.debugPrintObjectId = foundObjectId;
 							end;			
 						end;
 					end;
 				end;
 			else
-				self.debugPrintDone = false;
-				local x,y,z = localToWorld(g_currentMission.player.cameraNode, 0,0,1.0)
-				local dx,dy,dz = localDirectionToWorld(g_currentMission.player.cameraNode, 0,0,-1)
-				local distance = Player.MAX_PICKABLE_OBJECT_DISTANCE * 1.5
-				raycastAll(x,y,z, dx,dy,dz, "infoObjectRaycastCallback", distance, self)
+				local x,y,z = localToWorld(g_currentMission.player.cameraNode, 0,0,1.0);
+				local dx,dy,dz = localDirectionToWorld(g_currentMission.player.cameraNode, 0,0,-1);
+				local distance = Player.MAX_PICKABLE_OBJECT_DISTANCE * 2.2;
+				raycastAll(x,y,z, dx,dy,dz, "infoObjectRaycastCallback", distance, self);
 			end;
 		end;
 			
@@ -110,7 +116,7 @@ end;
 function GC_ObjectInfo:infoObjectRaycastCallback(hitObjectId, x, y, z, distance)
 	if (hitObjectId ~= g_currentMission.terrainDetailId) then
 		local locRigidBodyType = getRigidBodyType(hitObjectId);
-		if (getRigidBodyType(hitObjectId) ~= "NoRigidBody") then		
+		if (locRigidBodyType == "Dynamic") or (self.isMultiplayer and (locRigidBodyType == "Kinematic")) then
 			local object = g_currentMission:getNodeObject(hitObjectId);		
 			if (object~= nil) then
 				if (object.typeName == "pallet") or (object.typeName == "genetix") or (object.typeName == "attachablePallet") then
@@ -125,12 +131,20 @@ function GC_ObjectInfo:infoObjectRaycastCallback(hitObjectId, x, y, z, distance)
 							self.showInfo = true;
 						end;
 					end;
-				end;			
-				if not self.debugPrintDone then
+				elseif self.isMultiplayer and (object.typeName == nil) and (object.fillType ~= nil) and (object.fillLevel ~= nil) then
+					if object:isa(Bale) then
+						GC_ObjectInfo.mpFoundBale = object;
+					end;
+					self.displayLine1 = g_company.languageManager:getText('GC_ObjectInfo_filltype'):format(Utils.getNoNil(g_fillTypeManager.fillTypes[object.fillType].title,"unknown"));
+					self.displayLine2 = g_company.languageManager:getText('GC_ObjectInfo_level'):format(object.fillLevel);
+					self.displayLine3 = g_company.languageManager:getText('GC_ObjectInfo_owner'):format(GC_ObjectInfo:getFarmInfo(object.ownerFarmId));
+					self.showInfo = true;
+				end;
+				if (hitObjectId ~= self.debugPrintObjectId) then
 					g_company.debug:writeDevDebug(self.debugData, "hitObjectId = %s, locRigidBodyType = %s", hitObjectId, locRigidBodyType);
 					-- gc_debugPrint(object, nil, 1, "ObjectInfo");
 				end;
-				self.debugPrintDone = true;
+				self.debugPrintObjectId = hitObjectId;
 			end;
 		end;
 	end;
@@ -142,9 +156,10 @@ function GC_ObjectInfo:getFarmInfo(ownerFarmId)
 		local farm = g_farmManager:getFarmById(ownerFarmId);
 		if (farm ~= nil) then
 			farmName = farm.name;
+			return farmName;
 		end;
 	end;
-	return farmName
+	return farmName;
 end;
 
 g_company.addInit(GC_ObjectInfo, GC_ObjectInfo.init);
