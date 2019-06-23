6c80f02cbb4162f4b4438f4e72c79afe72e694de
aPuehri <48010391+aPuehri@users.noreply.github.com>
2019-06-23 20:37:13 +0200

    added bale owner information in multiplayer, added delay to fade out Info

--- additionals/GC_ObjectInfo.lua
+++ additionals/GC_ObjectInfo.lua
@@ -3,15 +3,17 @@
 --
 -- @Interface: --
 -- @Author: LS-Modcompany / aPuehri
--- @Date: 20.06.2019
+-- @Date: 23.06.2019
 -- @Version: 1.0.2.0
 --
 -- @Support: LS-Modcompany
 --
 -- Changelog:
 --
--- 	v1.0.2.0 (20.06.2019)/(aPuehri):
+-- 	v1.0.2.0 (23.06.2019)/(aPuehri):
 -- 		- changed client detection
+-- 		- added bale owner information in multiplayer
+-- 		- added delay to fade out Info
 --
 -- 	v1.0.1.0 (31.05.2019)/(aPuehri):
 -- 		- changed debugPrint
@@ -36,17 +38,22 @@ InitObjectClass(GC_ObjectInfo, "GC_ObjectInfo");
 
 GC_ObjectInfo.debugIndex = g_company.debug:registerScriptName("Gc_ObjectInfo");
 GC_ObjectInfo.foundBale = nil;
+GC_ObjectInfo.lastFoundBaleNetworkId = nil;
 
 function GC_ObjectInfo:init()
 	local self = setmetatable({}, GC_ObjectInfo_mt);
 
 	self.isServer = g_server ~= nil;
 	self.isClient = g_dedicatedServerInfo == nil;
-	self.isMultiplayer = g_currentMission.missionDynamicInfo.isMultiplayer;	
+	self.isMultiplayer = g_currentMission.missionDynamicInfo.isMultiplayer;		
 
 	self.debugPrintObjectId = 0;
+	self.fadeOutDelay = 0;
 	
 	self.debugData = g_company.debug:getDebugData(GC_ObjectInfo.debugIndex, g_company);
+
+	self.eventId_getFarmId = g_company.eventManager:registerEvent(self, self.getFarmIdEvent);
+	self.eventId_sendFarmId = g_company.eventManager:registerEvent(self, self.sendFarmIdEvent, true);
 
 	if self.isClient then
 		g_company.addUpdateable(self, self.update);	
@@ -82,9 +89,15 @@ function GC_ObjectInfo:update(dt)
 		else
 			self.gui.classGui:setData(self.displayLine1, self.displayLine2, self.displayLine3);
 		end;
-	elseif self.gui ~= nil then			
-		g_company.gui:closeGui("gcObjectInfo");
-		self.gui = nil;
+	elseif not self.showInfo and self.gui ~= nil then
+		self.fadeOutDelay = self.fadeOutDelay + (dt * 0.001);
+		if self.fadeOutDelay > 0.85 then
+			g_company.gui:closeGui("gcObjectInfo");
+			self.gui = nil;
+			self.fadeOutDelay = 0;
+		end;
+	elseif not self.showInfo and self.gui == nil and self.fadeOutDelay ~= 0 then
+		self.fadeOutDelay = 0;
 	end;
 end;
 
@@ -95,6 +108,7 @@ function GC_ObjectInfo:infoObjectRaycastCallback(hitObjectId, x, y, z, distance)
 			local object = g_currentMission:getNodeObject(hitObjectId);		
 			if (object~= nil) then
 				if (object.typeName == "pallet") or (object.typeName == "genetix") or (object.typeName == "attachablePallet") then
+					GC_ObjectInfo.foundBale = nil;
 					if (object.getFillUnits ~= nil) then
 						local fUnit = object:getFillUnits();
 						if object:getFillUnitExists(fUnit[1].fillUnitIndex) then							
@@ -102,16 +116,16 @@ function GC_ObjectInfo:infoObjectRaycastCallback(hitObjectId, x, y, z, distance)
 							local perc = Utils.getNoNil(g_company.mathUtils.round((object:getFillUnitFillLevelPercentage(fUnit[1].fillUnitIndex) * 100),0.01),0);
 							self.displayLine1 = g_company.languageManager:getText('GC_ObjectInfo_filltype'):format(Utils.getNoNil(g_fillTypeManager.fillTypes[fUnit[1].fillType].title,"unknown"));
 							self.displayLine2 = g_company.languageManager:getText('GC_ObjectInfo_level2'):format(lev, perc);
-							self.displayLine3 = g_company.languageManager:getText('GC_ObjectInfo_owner'):format(GC_ObjectInfo:getFarmInfo(object.ownerFarmId));
+							self.displayLine3 = g_company.languageManager:getText('GC_ObjectInfo_owner'):format(GC_ObjectInfo:getFarmInfo(object, self, false));
 							self.showInfo = true;
 						end;
 					end;
 				elseif (object.typeName == nil) and (object.fillType ~= nil) and (object.fillLevel ~= nil) then
 					if object:isa(Bale) then
-						GC_ObjectInfo.foundBale = object;						
+						GC_ObjectInfo.foundBale = object;
 						self.displayLine1 = g_company.languageManager:getText('GC_ObjectInfo_filltype'):format(Utils.getNoNil(g_fillTypeManager.fillTypes[object.fillType].title,"unknown"));
 						self.displayLine2 = g_company.languageManager:getText('GC_ObjectInfo_level'):format(object.fillLevel);
-						self.displayLine3 = g_company.languageManager:getText('GC_ObjectInfo_owner'):format(GC_ObjectInfo:getFarmInfo(object.ownerFarmId));
+						self.displayLine3 = g_company.languageManager:getText('GC_ObjectInfo_owner'):format(GC_ObjectInfo:getFarmInfo(object, self, false));
 						self.showInfo = true;						
 					end;
 				end;
@@ -127,16 +141,52 @@ function GC_ObjectInfo:infoObjectRaycastCallback(hitObjectId, x, y, z, distance)
 	end;
 end;
 
-function GC_ObjectInfo:getFarmInfo(ownerFarmId)
+function GC_ObjectInfo:getFarmInfo(object, ref, noEventSend)
+	local self = ref;
 	local farmName = "unknown";
-	if (ownerFarmId ~= nil) then
-		local farm = g_farmManager:getFarmById(ownerFarmId);
+	local farmId = nil;
+
+	if self.isClient and self.isMultiplayer and object:isa(Bale) then
+		self.foundBaleNetworkId = NetworkUtil.getObjectId(object);
+		
+		if GC_ObjectInfo.lastFoundBaleNetworkId ~= self.foundBaleNetworkId then
+			GC_ObjectInfo.lastFoundBaleNetworkId = self.foundBaleNetworkId;
+			self.mpBaleOwnerId = nil;
+			self:getFarmIdEvent({self.foundBaleNetworkId}, noEventSend);			
+		end;
+		farmId = self.mpBaleOwnerId;
+	else
+		farmId = object.ownerFarmId;
+	end;
+	
+	if (farmId ~= nil) then
+		local farm = g_farmManager:getFarmById(farmId);
 		if (farm ~= nil) then
 			farmName = farm.name;
 			return farmName;
 		end;
 	end;
 	return farmName;
+end;
+
+function GC_ObjectInfo:getFarmIdEvent(data, noEventSend)
+	g_company.eventManager:createEvent(self.eventId_getFarmId, data, false, noEventSend);
+	
+	if self.isServer then
+		local baleObject = NetworkUtil.getObject(data[1]);
+		local farm = g_farmManager:getFarmById(baleObject.ownerFarmId);
+		if (farm ~= nil) then
+			self:sendFarmIdEvent({baleObject.ownerFarmId});
+		end;
+	end;
+end;
+
+function GC_ObjectInfo:sendFarmIdEvent(data, noEventSend)   
+	if self.isServer then
+		g_company.eventManager:createEvent(self.eventId_sendFarmId, data, false, noEventSend);
+	else
+		self.mpBaleOwnerId = data[1];
+	end;
 end;
 
 g_company.addInit(GC_ObjectInfo, GC_ObjectInfo.init);
