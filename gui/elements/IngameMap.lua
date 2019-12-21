1555e6046e7a9fb0f157904c4481c2a1cf5f3d81
kevink98 <36474411+kevink98@users.noreply.github.com>
2019-12-21 19:51:50 +0100

    Add PdaMarker

--- gui/elements/IngameMap.lua
+++ gui/elements/IngameMap.lua
@@ -31,6 +31,18 @@ function GC_Gui_ingameMap:new(gui, custom_mt)
 	self.lastSize = self.zoomFactor * 128
 
 	self.bitmaps = {}
+	self.pdaMarkerCount = -1
+	self.pdaMarkers = {}
+
+	self.pdaWith = 2048
+	if fileExists(g_currentMission.missionInfo.baseDirectory.."modDesc.xml") then
+		local path = g_currentMission.missionInfo.baseDirectory .. g_currentMission.missionInfo.mapXMLFilename
+		if fileExists(path) then
+			local xml = loadXMLFile("map",path,"map")
+			self.pdaWith = getXMLInt(xml, "map#width")
+			delete(xml)
+		end
+	end
 	
 	return self;
 end;
@@ -70,8 +82,7 @@ end;
 function GC_Gui_ingameMap:mouseEvent(posX, posY, isDown, isUp, button, eventUsed)
 	if not self:getDisabled() then
 		eventUsed = GC_Gui_ingameMap:superClass().mouseEvent(self, posX, posY, isDown, isUp, button, eventUsed)
-		
-		
+				
 		if not eventUsed then
 
 			local clickZone = {};		
@@ -161,6 +172,7 @@ function GC_Gui_ingameMap:zoom(value, posX, posY)
 	self.lastPxPosY = self:checkEdges(self.lastPxPosY)
 	
 	self.overlayElement:setUV(string.format("%spx %spx %spx %spx", self.lastPxPosX, self.lastPxPosY, self.lastSize, self.lastSize))
+	self:checkPdaMarkers()
 end
 
 function GC_Gui_ingameMap:checkEdges(lastPos)
@@ -180,6 +192,7 @@ function GC_Gui_ingameMap:move(posX, posY)
 	self.lastPxPosY = self:checkEdges(self.lastPxPosY)
 
 	self.overlayElement:setUV(string.format("%spx %spx %spx %spx", self.lastPxPosX, self.lastPxPosY, self.lastSize, self.lastSize))
+	self:checkPdaMarkers()
 end
 
 function GC_Gui_ingameMap:registerBitmap(name, bitmapId, state)
@@ -224,6 +237,42 @@ function GC_Gui_ingameMap:draw(index)
 				setOverlayUVs(data.overlay, unpack(GuiUtils.getUVs(string.format("%spx %spx %spx %spx", self.lastPxPosX, self.lastPxPosY, self.lastSize, self.lastSize), self.imageSize, nil)));	
 				renderOverlay(data.overlay, self.drawPosition[1], self.drawPosition[2], self.size[1], self.size[2]);
 			end
+		end
+	end
+end
+
+function GC_Gui_ingameMap:addPdaMarker(element)
+	self.pdaMarkerCount = self.pdaMarkerCount + 1
+	table.insert(self.pdaMarkers, {id = self.pdaMarkerCount, element = element, posX = 0, posY = 0, worldPosX = 0, worldPosY = 0, size = element.size})
+	return self.pdaMarkerCount
+end
+
+function GC_Gui_ingameMap:setPdaMarkerPosition(id, x, y)
+	for _,marker in pairs(self.pdaMarkers) do
+		if marker.id == id then
+			marker.worldPosX = x
+			marker.worldPosY = y
+		end
+	end
+	self:checkPdaMarkers()
+end
+
+function GC_Gui_ingameMap:checkPdaMarkers()	
+	for _,marker in pairs(self.pdaMarkers) do
+		local sizeH = marker.size[1] / 2
+		
+		local pdaPosX = (marker.worldPosX + self.pdaWith / 2) / 2
+		local pdaPosY = (marker.worldPosY + self.pdaWith / 2) / 2
+		
+		if self.lastPxPosX < pdaPosX and pdaPosX < (self.lastPxPosX + self.lastSize) and self.lastPxPosY < pdaPosY and pdaPosY < (self.lastPxPosY + self.lastSize)  then
+
+			local posX = (880 / self.lastSize * (pdaPosX - self.lastPxPosX)) - 440
+			local posY = 440 - (880 / self.lastSize * (pdaPosY - self.lastPxPosY))
+
+			marker.element.position = GuiUtils.getNormalizedValues(string.format("%spx %spx", posX, posY), marker.element.outputSize, marker.element.position)
+			marker.element:setVisible(true)
+		else
+			marker.element:setVisible(false)
 		end
 	end
 end
